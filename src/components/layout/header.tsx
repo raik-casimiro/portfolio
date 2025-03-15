17fb33133f38ea38682db859d585ff2c2a814c9c
Raik Casimiro <raikyuri@gmail.com>
2025-03-15 00:37:15 -0300

    feat: enhance header component layout with responsive sidebar links and icons

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -9,7 +9,8 @@ export default function Header() {
     const { hash, updateHash } = useHash({ defaultHash: "#about" });
 
     return (
-        <div className="flex bg-background text-muted-foreground">
+        <div className="flex justify-between bg-background text-muted-foreground">
+            <div className="flex">
             {data.header.pages.map((page) => {
                 const isActive = page.hash === hash || (page.hash === '#about' && !hash);
                 return (
@@ -17,17 +18,31 @@ export default function Header() {
                         key={page.hash}
                         href={page.hash}
                         onClick={() => updateHash(page.hash)}
-                        className={classNames('relative px-4 py-2 text-sm font-medium flex items-center transition-all duration-150', {
+                        className={classNames('relative px-4 py-2 text-sm font-medium flex items-center gap-2 transition-all duration-150', {
                             "bg-muted text-foreground border-b-0": isActive,
                             "hover:bg-muted hover:text-foreground": !isActive
                         })}
                     >
-                        {page.label}
+                        <page.icon />
+                        <span className="hidden md:block">{page.label}</span>
                         {isActive && <div className="absolute top-0 left-0 w-full h-0.5 bg-highlight" />}
                         {isActive && <div className="absolute -bottom-0.5 left-0 w-full h-1 bg-muted" />}
                     </Link>
                 );
-            })}
+            })} 
+            </div>
+            <div className="flex md:hidden">
+                {data.sidebar.links.map((link, index) => ( 
+                    <Link 
+                        key={`header_link_${link.name}_${index}`}
+                        href={link.href} 
+                        target="_blank" 
+                        className="px-4 py-2 hover:bg-muted hover:text-foreground transition duration-300 flex justify-center"
+                    >
+                        <link.icon size={18} />
+                    </Link> 
+                ))} 
+            </div>
         </div>
     );
 }
